d6bf925fcae1fced6e0e1c53d85fd2e343c86185
Turtle923 <41129005+Turtle923@users.noreply.github.com>
2019-08-10 19:33:53 +0500

    Update enter program.py

--- enter program.py
+++ enter program.py
@@ -4,32 +4,52 @@ import time
 from py2 import PressKey,ReleaseKey,W,S,A,D,C,Enter
 import keyboard
 from PIL import ImageGrab
-
-newimg = cv2.imread('matchimage1.png')
-th1 = cv2.Canny(newimg,threshold1 =200,threshold2 = 300)
-
-#cv2.imshow('123',grayimg1)
+#time.sleep(2)
+th1 = cv2.imread('matchimage1.png')
+#th1 = cv2.Canny(newimg,threshold1 =200,threshold2 = 300)
+#th1 =cv2.cvtColor(newimg, cv2.COLOR_BGR2RGB)
+i = 0
+#cv2.imshow('abc',th1)
+global start_timer
+start_timer = time.time()
 while True:
+    i = i + 1
     if keyboard.is_pressed('esc'):
+        
+        elapsed = time.time() - start_timer
+        print((elapsed/60)/60,"Hour")
+        print(elapsed/60,"Min")
+        print(elapsed,"Sec")
         break
-    time.sleep(0)
+
+    #time.sleep(0)
     Print_Screen = ImageGrab.grab()
-   # Print_Numpy = np.array(Print_Screen.getdata(),dtype='uint8')\
-    #.reshape((Print_Screen.size[1],Print_Screen.size[0],3))
     Print_Numpy = np.array(Print_Screen)
     x,y,w,h = bbox = (600, 460, 650, 150)
     croped = Print_Numpy[y:y+h, x:x+w]
-    th2 = cv2.Canny(croped,threshold1 =200,threshold2 = 300)
-    #cv2.imshow('abc',th2)
+    #th2 = cv2.Canny(croped,threshold1 =200,threshold2 = 300)
+    th2 = cv2.cvtColor(croped, cv2.COLOR_BGR2RGB)
+    #cv2.imshow('abc1',th2)
     iar = np.array(th1)
     iar2 = np.array(th2)
-
-    if (iar == iar2).all():
+    matched_arrays = 0
+    x = 0      
+    while (x < 150):
+        y = 0
+        while (y < 650 ):   
+            if (iar[x][y] == iar2[x][y]).all():
+                #print ("match")
+                matched_arrays = matched_arrays + 1
+            y  = y + 1
+        x = x + 1
+        if x == iar.size:
+            break
+    percent = (matched_arrays / 97500) * 100
+    if percent > 70:
         PressKey(Enter)
+        print(percent)
         print("Matched")
         time.sleep(3)
-    else:
-        print("Not mATched")
-
+print(i)
 
 
